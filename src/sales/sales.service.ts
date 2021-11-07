abd064acaed5e85b681be231d9d6a83081311187
cspark0610 <75094281+cspark0610@users.noreply.github.com>
2021-11-06 23:33:15 -0300

    Update sales.service.ts

--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { ResultTemplate } from './entities/result-template.entity.dto';
-import { SaleResult } from './entities/sale-result.entity';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { ResultTemplate } from "./entities/result-template.entity.dto";
+import { SaleResult } from "./entities/sale-result.entity";
 
 @Injectable()
 export class SalesService {
@@ -9,32 +9,40 @@ export class SalesService {
 
   async updateMovieStockAndCreateSaleRecord(body): Promise<ResultTemplate | SaleResult> {
     const { movieId, customerEmail } = body;
-    const movie = await this.prisma.movie.findUnique({
+    const movie = await this.prisma.movie.findFirst({
       where: {
-        id: movieId
-      }
+        id: movieId,
+      },
     });
-    if (movie.available) {
+    if (movie.stock > 0) {
       const newStock = movie.stock - 1;
       const newSaleRecord = { customerEmail, price: movie.salePrice };
       const newSale = await this.prisma.sale.create({
         data: {
           ...newSaleRecord,
-          movieId: { connect: { id: movieId } }
-        }
+          movieId: { connect: { id: movieId } },
+        },
       });
 
       const updateResult = await this.prisma.movie.updateMany({
         where: { id: movie.id },
-        data: { stock: newStock < 0 ? 0 : newStock }
+        data: { stock: newStock < 0 ? 0 : newStock },
       });
       if (newSale && updateResult.count)
         return {
           movieId: movie.id,
           customerEmail,
-          price: movie.salePrice
+          price: movie.salePrice,
         };
+    } else {
+      // in case stock is 0 update movie availaible to false
+      await this.prisma.movie.updateMany({
+        where: { id: movie.id },
+        data: { available: false },
+      });
+
+      return { success: false, statusCode: 400, message: "bad request movie is not available to sell" };
     }
-    return { success: false, statusCode: 400, message: 'bad request movie is not available to sell' };
   }
 }
+
